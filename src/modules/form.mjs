a4311527a4a7d67315f9499927e9c98c9d6f12fa
Nicole <nicole.silfverling@gmail.com>
2024-12-03 13:20:17 +0100

    validate name function

--- src/modules/form.mjs
+++ src/modules/form.mjs
@@ -5,13 +5,31 @@ export function validateForm(e, checkoutForm) {
   const fname = formData.get("fname");
   const lname = formData.get("lname");
 
-  if (fname.length < 2) {
-    console.error("Firstname is required.");
-  }
-  if (lname.length < 2) {
-    console.error("Lastname is required.");
-  }
+  console.error("First name: " + validateName(fname));
+  console.error("Last name: " + validateName(lname));
 
   console.log(fname);
   console.log(lname);
+}
+
+//----------------------------------------------------
+/**
+ * Validates name and returns error message
+ * @param {*} name
+ * @returns String with error message
+ */
+
+function validateName(name) {
+  if (!name || name.trim() === "") {
+    //if no input or only whitespaces
+    return "Name is required.";
+  } else if (name.length < 2) {
+    return "Name must be at least 2 characters long.";
+  } else if (name.length > 50) {
+    return "Name must be less than 50 characters long.";
+  } else if (!/^[a-öA-Ö\s'-]+$/.test(name)) {
+    // allows letters A-Ö, spaces, ' and -
+    return "Name can only include letters, spaces, and hyphens.";
+  }
+  return null;
 }
